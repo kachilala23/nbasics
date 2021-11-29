a94884a2df974bb293c4e94792407f874615d879
kachilala <kachilala23@gmail.com>
2021-11-29 16:08:40 +0100

    using arguement with both typescript and graphql

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Boolean) 
-  isPizzaGood(): Boolean  {
-    return false;
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
