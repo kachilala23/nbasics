c542409ab0a0d8db6a2c270df7277a931f553b96
kachilala <kachilala23@gmail.com>
2021-11-29 16:37:38 +0100

    create dto, class-transformer, class-validator and global validation pipeline

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,19 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
+
   @Query(returns => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
     return [];
   }
+
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+  
 }
